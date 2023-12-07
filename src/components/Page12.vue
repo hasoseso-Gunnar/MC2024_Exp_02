f4734c54c4cc0eb7c6e909a1153e14e4ef70468b
Takumi Gunnar Miura <54161086+hasoseso-Gunnar@users.noreply.github.com>
2023-12-07 15:48:46 +0900

    Update Page12.vue

--- src/components/Page12.vue
+++ src/components/Page12.vue
@@ -2,7 +2,7 @@
   <div class="q-pa-md">
     <p>※このページは20秒経過すると、ページ下部の「ゲームを開始する」ボタンから次の画面に進むことができます。</p>
     <br/>
-    <p class="text-subtitle1 text-black">これは、先ほどの質問項目でのパートナーの回答内容です。</p>
+    <p class="text-subtitle1 text-black">これは、先ほどの質問項目での<span class="text-bold text-red-9">パートナーの回答内容</span>です。</p>
     <p class="text-subtitle1 text-black text-bold">プロフィール情報の一部について、ゲームの後で正しく記憶しているかどうか確認しますので、しっかりと覚えてください。</p>
     <div style="height: 40px;"></div>
     <p class="text-h6 text-black text-bold" style="margin-left: 20%;">【パートナーの情報】</p>
